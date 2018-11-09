f3157864817c58b894eecc2732c06eb0421fce6d
Chris Gerth <Gerth_Christopher_M@cat.com>
2018-11-08 19:53:05 -0600

    Added initial intake class.

---
+++ src/main/java/frc/robot/Intake.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Intake{
+
+    //Constructor
+    /** another comment
+     * comment
+     * 
+     * comment mc comment face
+     */
+    public Intake(){
+
+    }
+
+}
