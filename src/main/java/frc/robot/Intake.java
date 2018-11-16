b76145b0a3c060ab3c1bb88bc8c1068c240405de
joshua <joshua@1736.org>
2018-11-15 19:54:04 -0600

    added commands to lower elbow and added thing that checks if cube is in the thing

--- src/main/java/frc/robot/Intake.java
+++ src/main/java/frc/robot/Intake.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class Intake{
 
@@ -10,15 +11,36 @@ public class Intake{
     private XboxController xboxCont;
     Spark leftMotor;
     Spark rightMotor;
+    Spark elbowMotor;
+    DigitalInput sensor;
+    Boolean cubeHeld = false;
+    Boolean elbowMotorRaised = true;
+
+
+
     public Intake(){
         xboxCont = new XboxController(0);
         leftMotor = new Spark( 0);
         rightMotor = new Spark( 1);
+        sensor = new DigitalInput(3);
+        elbowMotor = new Spark(8);
     }
     //init stuff
     public void update() {
         double leftMotorCmd;
-        double rightMotorCmd;
+        double rightMotorCmd; 
+        if(elbowMotorRaised == true){
+            elbowMotor.set(0);
+            //we're not sure about 0
+            elbowMotorRaised = false;
+        }
+        
+        if(sensor.get() == true) {
+             cubeHeld = true;
+        }else{ 
+            cubeHeld = false;
+
+        }
 
 
         boolean Abutton = xboxCont.getAButton();
@@ -36,6 +58,7 @@ public class Intake{
         System.out.println(Abutton);
         leftMotor.set(leftMotorCmd);
         rightMotor.set(-1*rightMotorCmd);
+
     }
 
 }
