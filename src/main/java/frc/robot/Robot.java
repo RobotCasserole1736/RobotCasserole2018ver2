f20d5ba6648071f5724b1e7c4dfef0f003e3547f
Aden <adenharriman@gmail.com>
2018-11-27 19:50:15 -0600

    Removed unnecessary line.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -22,7 +22,6 @@ import frc.robot.DriveTrain;
 public class Robot extends TimedRobot {
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
   Intake intk;
   DriveTrain DT;
